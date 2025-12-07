08feb9f27066b7e68c16aeac7fb9309d6519ae5a
Ryo Watanabe <watanabe.ryo@gmail.com>
2025-12-07 19:47:56 +0900

    added pages for OBS

---
+++ export/recent_updates.js
@@ -0,0 +1,158 @@
+// 「最近」として表示する履歴の範囲 (単位:時間)
+const timeWindow = 12;
+
+// 更新のあるリザルトのみに表示を絞るか
+const showUpdatedOnly = true;
+
+let url = null;
+let socket = null;
+let musictable = null;
+let looprequest = null;
+
+async function connect() {
+    console.log("connecting...");
+    if(socket != null) return;
+
+    socket = new WebSocket(url);
+
+    socket.addEventListener('open', (event) => {
+        console.log("websocket opened.");
+        socket.send('get_musictable');
+    });
+
+    socket.addEventListener('message', (event) => {
+        console.log("message received.");
+        if(typeof event.data === 'string') {
+            try {
+                let data = JSON.parse(event.data);
+                if (data.method == 'get_musictable') {
+                    musictable = data.result;
+                    // 楽曲リストをロードできたら、履歴表示を開始
+                    clearInterval(looprequest);
+                    $("#setting").css("display", "none");
+                    $("#content").css("display", "block");
+                    looprequest = setInterval(loadJson, 1000);
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    });
+
+    socket.addEventListener('error', (event) => {
+        console.log("websocket error:");
+        console.log(event);
+        socket.close();
+    });
+
+    socket.addEventListener('close', (event) => {
+        console.log("websocket closed.");
+        socket = null;
+    });
+}
+
+function loadJson() {
+    let getjson = $.ajax({
+        url: '../records/recent.json',
+        type: 'GET',
+        dataType: 'json',
+        cache: false
+    });
+
+    getjson.done(function(json){
+        let out = "<div></div><div class='header'>Music</div><div class='header'>Lamp</div><div class='header'>Score</div><div class='header'>BP</div>";
+        let timestamps = json["timestamps"];
+
+        // 履歴の足切り日時の文字列を yyyymmdd-hhmmss 形式で取得
+        let now = new Date();
+        let threshold = new Date(now.getTime() - timeWindow * 60 * 60 * 1000);
+        let yyyy = threshold.getFullYear().toString();
+        let mm = (threshold.getMonth() + 1).toString().padStart(2, '0');
+        let dd = threshold.getDate().toString().padStart(2, '0');
+        let hh = threshold.getHours().toString().padStart(2, '0');
+        let min = threshold.getMinutes().toString().padStart(2, '0');
+        let ss = threshold.getSeconds().toString().padStart(2, '0');
+        let timestamp_threshold = yyyy + mm + dd + '-' + hh + min + ss;
+
+        timestamps.filter(ts => ts > timestamp_threshold).sort((a, b) => b.localeCompare(a)).forEach(function(ts){
+            let entry = json["results"][ts];
+
+            if (showUpdatedOnly && !entry["update_clear_type"] && !entry["update_dj_level"] && !entry["update_score"] && !entry["update_miss_count"]) {
+                return;
+            }
+
+            let difficulty = entry["difficulty"];
+            let playtype = entry["playtype"];
+            let title = entry["music"];
+            let playspeed = entry["playspeed"];
+            let lamp = entry["update_clear_type"];
+            let score = entry["update_score"];
+            let bp = entry["update_miss_count"];
+            let options = entry["option"];
+            
+            let level = null;
+            try {
+                if (playtype == "DP") {
+                    level = musictable["musics"][title]["DP"][difficulty];
+                } else {
+                    // DBの難易度はSP準拠
+                    level = musictable["musics"][title]["SP"][difficulty];
+                }
+            } catch(e) {
+            }
+
+            // DB系のプレイオプションを反映
+            let db_options = "";
+            if (playtype === "DP BATTLE") {
+                playtype = 'DP'
+                if (options.indexOf("A-SCR")<0){
+                    db_options = "皿あり";
+                }
+                if ((options.indexOf("MIR/OFF")>=0) || (options.indexOf("OFF/MIR")>=0)){
+                    db_options += 'DBM'
+                }
+                else if (options.indexOf("OFF/OFF")>=0){
+                    db_options += 'DB'
+                }
+                else if (options.indexOf("RAN/RAN")>=0){
+                    db_options += 'DBR'
+                }
+                else if (options.indexOf("S-RAN/S-RAN")>=0){
+                    db_options += 'DBSR'
+                }
+                else if (options.indexOf("H-RAN/H-RAN")>=0){
+                    db_options += 'DBHR'
+                }
+            }
+
+            title = `${title} (${playtype + difficulty.slice(0,1)})`
+            if (playspeed) {
+                title = `<span class="plain">(x${playspeed})</span> ${title}` 
+            }
+            if (db_options != ""){
+                title = `<span class="plain">(${db_options})</span> ${title}`;
+            }
+
+
+            out += `<div class="level">${level === null ? '' : '☆' + level}</div>`
+            out += `<div class="title ${difficulty}">${title}</div>`
+            out += `<div class="lamp ${lamp}">${lamp === null ? '' : lamp}</div>`
+            out += `<div class="score">${score === null ? '' : "+" + score}</div>`;
+            out += `<div class="miss_count">${bp === null ? '' : bp}</div>`;
+        });
+        $('#result').html(out);
+    });
+
+    getjson.fail(function(err) {
+        // alert('failed');
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    const cssValue = getComputedStyle($(':root')[0]).getPropertyValue('--ws-url').trim();
+    if(cssValue.length)
+        url = cssValue.replace(/^["']|["']$/g, '');
+    $('span#url').text(url);
+
+    looprequest = setInterval(connect, 5000);
+});
---
+++ export/score_results.js
@@ -0,0 +1,159 @@
+// 「最近」として表示する履歴の範囲 (単位:時間)
+const timeWindow = 168;
+
+// 更新のあるリザルトのみに表示を絞るか
+const showUpdatedOnly = false;
+
+let url = null;
+let socket = null;
+let musictable = null;
+let looprequest = null;
+
+async function connect() {
+    console.log("connecting...");
+    if(socket != null) return;
+
+    socket = new WebSocket(url);
+
+    socket.addEventListener('open', (event) => {
+        console.log("websocket opened.");
+        socket.send('get_musictable');
+    });
+
+    socket.addEventListener('message', (event) => {
+        console.log("message received.");
+        if(typeof event.data === 'string') {
+            try {
+                let data = JSON.parse(event.data);
+                if (data.method == 'get_musictable') {
+                    musictable = data.result;
+                    clearInterval(looprequest);
+                    looprequest = setInterval(function(){
+                        try {
+                            socket.send('get_scoreresult');
+                        } catch(e) {
+                            console.log(e);
+                            $("#setting").css("display", "flex");
+                            $("#content").css("display", "none");
+                            clearInterval(looprequest);
+                            looprequest = setInterval(connect, 1000);
+                        }
+                    }, 1000);
+                } else if (data.method == 'get_scoreresult') {
+                    // スコアリストをロードできたら、履歴表示を開始
+                    $("#setting").css("display", "none");
+                    $("#content").css("display", "block");
+                    applyData(data);
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    });
+
+    socket.addEventListener('error', (event) => {
+        console.log("websocket error:");
+        console.log(event);
+        socket.close();
+    });
+
+    socket.addEventListener('close', (event) => {
+        console.log("websocket closed.");
+        socket = null;
+    });
+}
+
+function optionToString(options){
+    if (options === null) {
+        return "???";
+    }
+    let out = [];
+    if (options.arrange !== null) {
+        out.push(options.arrange);
+    }
+    if (options.flip !== null) {
+        out.push(options.flip);
+    }
+    if (options.battle) {
+        out.push("BATTLE");
+    }
+    if (options.assist !== null) {
+        out.push(options.assist);
+    }
+    return out.join(",");
+}
+
+function applyData(data) {
+    if (!data["result"]) {
+        return;
+    }
+    let timestamps = data["result"]["timestamps"];
+    let title = data["music"]["musicname"];
+    let difficulty = data["music"]["difficulty"];
+    let playtype = data["music"]["playtype"];
+    let level = null;
+    try {
+        if (playtype == "DP") {
+            level = musictable["musics"][title]["DP"][difficulty];
+        } else {
+            // DBの難易度はSP準拠
+            level = musictable["musics"][title]["SP"][difficulty];
+        }
+    } catch(e) {
+    }
+
+    $("#title").html(`☆${level} ${title} (${playtype == 'DP BATTLE' ? 'DB' : playtype}${difficulty.slice(0,1)})`);
+    
+    let best_score = "???";
+    let best_score_opt = "???";
+    if (data["result"]["best"]["score"] !== undefined) {
+        best_score = data["result"]["best"]["score"]["value"];
+        best_score_opt = optionToString(data["result"]["best"]["score"]["options"]);        
+    }
+    let best_bp = "???";
+    let best_bp_opt = "???";
+    if (data["result"]["best"]["miss_count"] !== undefined) {
+        best_bp = data["result"]["best"]["miss_count"]["value"];
+        best_bp_opt = optionToString(data["result"]["best"]["miss_count"]["options"]);
+    }
+    let best_html = "";
+    best_html += `<div class="score">best(score)</div><div class="score value">${best_score}</div><div class="options">${best_score_opt}</div>`;
+    best_html += `<div class="bp">best(bp)</div><div class="bp value">${best_bp}</div><div class="options">${best_bp_opt}</div>`;
+    $('#best').html(best_html);
+
+    let out = "";
+    timestamps.sort((a, b) => b.localeCompare(a)).forEach(function(ts){
+        let entry = data["result"]["history"][ts];
+        
+        let date = ts.slice(0,4) + '-' + ts.slice(4,6) + '-' + ts.slice(6,8);
+        let lamp = entry["clear_type"]["value"];
+        let rank = entry["dj_level"]["value"];
+        let score = entry["score"]["value"];
+        let playspeed = entry["playspeed"] ? `x${entry["playspeed"]}` : "";
+        let opt = optionToString(entry["options"]);
+        let bp = entry["miss_count"]["value"];
+        if (bp === null) {
+            bp = "???";
+        }
+
+        // テーブルに追加
+        out += `<div class="date">${date}</div>`;
+        out += `<div><span class="${lamp}">　</span></div>`;
+        out += `<div class="rank ${rank}">${rank}</div>`;
+        out += `<div class="score value">${score}</div>`;
+        out += `<div class="bp value">${bp}</div>`;
+        out += `<div class="plain">${playspeed}</div>`;
+        out += `<div class="options">${opt}</div>`;
+
+    });
+    $('#result').html(out);
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    const cssValue = getComputedStyle($(':root')[0]).getPropertyValue('--ws-url').trim();
+    if(cssValue.length)
+        url = cssValue.replace(/^["']|["']$/g, '');
+    $('span#url').text(url);
+
+    looprequest = setInterval(connect, 1000);
+});
